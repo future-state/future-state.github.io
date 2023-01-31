f944ea7d7d584123fd081bcc5a578935a696e12b
Sam Johnson <idusortus@gmail.com>
2023-01-31 13:27:43 -0600

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://future-state.github.io',
+    // base: '/static-github-site',
+  })
